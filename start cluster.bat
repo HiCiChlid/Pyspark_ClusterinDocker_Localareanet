31c1f8755916acaaf502f234418b2bf2ceea030d
HiCiChlid <43268820+HiCiChlid@users.noreply.github.com>
2022-05-03 14:47:54 +0800

    Update start cluster.bat

--- start cluster.bat
+++ start cluster.bat
@@ -1,2 +1,2 @@
-ssh 192.168.0.3 "docker start geosci-env_M40;  docker exec geosci-env_M40 /root/init.sh; docker start slave_41;  docker exec slave_41 /root/init.sh"
-ssh 192.168.0.2 "docker start slave_30; docker exec slave_30 /root/init.sh; docker start slave_31; docker exec slave_31 /root/init.sh"
+ssh 192.168.0.3 "docker start geosci-env_M40;  docker exec geosci-env_M40 /root/init.sh; docker start slave41;  docker exec slave41 /root/init.sh"
+ssh 192.168.0.2 "docker start slave30; docker exec slave30 /root/init.sh; docker start slave31; docker exec slave31 /root/init.sh"
