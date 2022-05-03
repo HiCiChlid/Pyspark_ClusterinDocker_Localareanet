c1fd12f4d1970080f8953d7a9fb98c463b6de90c
HiCiChlid <43268820+HiCiChlid@users.noreply.github.com>
2022-05-03 14:48:17 +0800

    Update stop cluster .bat

--- stop cluster .bat
+++ stop cluster .bat
@@ -1,2 +1,2 @@
-ssh 192.168.0.3 "docker stop geosci-env_M40; docker stop slave_41"
-ssh 192.168.0.2 "docker stop slave_30; docker stop slave_31"
+ssh 192.168.0.3 "docker stop geosci-env_M40; docker stop slave41"
+ssh 192.168.0.2 "docker stop slave30; docker stop slave31"
